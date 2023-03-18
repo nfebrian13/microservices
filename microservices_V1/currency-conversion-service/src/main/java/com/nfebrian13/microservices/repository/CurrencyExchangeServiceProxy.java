a6f4e0354f7c54b9d64da6f04879bc4fc8d431c5
nfebrian13 <nanaz.febrian@gmail.com>
2023-03-18 11:36:21 +0700

    added configuration load balancer using ribbon

--- microservices_V1/currency-conversion-service/src/main/java/com/nfebrian13/microservices/repository/CurrencyExchangeServiceProxy.java
+++ microservices_V1/currency-conversion-service/src/main/java/com/nfebrian13/microservices/repository/CurrencyExchangeServiceProxy.java
@@ -1,14 +1,16 @@
 package com.nfebrian13.microservices.repository;
 
 
-import com.nfebrian13.microservices.bean.CurrencyConversionBean;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-services", url = "localhost:8000")
-//@RibbonClient(name = "currency-exchange-services")
+import com.nfebrian13.microservices.bean.CurrencyConversionBean;
+
+//@FeignClient(name = "currency-exchange-services", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-services")
+@RibbonClient(name = "currency-exchange-services")
 public interface CurrencyExchangeServiceProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
