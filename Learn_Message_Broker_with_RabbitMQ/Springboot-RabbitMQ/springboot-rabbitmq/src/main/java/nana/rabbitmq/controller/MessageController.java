807e29b84262e73dc51efb9773377c643c519eca
nfebrian13 <nanaz.febrian@gmail.com>
2023-06-30 22:02:58 +0700

    springboot rabbit mq tutorial

---
+++ Learn_Message_Broker_with_RabbitMQ/Springboot-RabbitMQ/springboot-rabbitmq/src/main/java/nana/rabbitmq/controller/MessageController.java
@@ -0,0 +1,24 @@
+package nana.rabbitmq.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import nana.rabbitmq.publisher.RabbitMQProducer;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+	@Autowired
+	private RabbitMQProducer producer;
+
+	@GetMapping("/publish")
+	public ResponseEntity<String> sendMessage(@RequestParam("message") String message) {
+		producer.sendMessage(message);
+		return ResponseEntity.ok("Message sent to RabbitMQ...");
+	}
+}
---
+++ Learn_Message_Broker_with_RabbitMQ/Springboot-RabbitMQ/springboot-rabbitmq/src/main/java/nana/rabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,28 @@
+package nana.rabbitmq.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+	@Value("${rabbitmq.exchange.name}")
+	private String exchange;
+
+	@Value("${rabbitmq.routing.key}")
+	private String routingKey;
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public void sendMessage(String message) {
+		LOGGER.info(String.format("Message sent -> ", message));
+		rabbitTemplate.convertAndSend(exchange, routingKey, message);
+	}
+}
