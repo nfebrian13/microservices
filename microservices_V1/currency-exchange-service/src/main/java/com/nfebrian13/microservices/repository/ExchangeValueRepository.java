71fda8271c9f108f03b7b308181512c3d8414d66
nfebrian13 <nanaz.febrian@gmail.com>
2023-07-09 11:15:03 +0700

    delete unused folder

--- microservices_V1/currency-exchange-service/src/main/java/com/nfebrian13/microservices/repository/ExchangeValueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nfebrian13.microservices.repository;
-
-import com.nfebrian13.microservices.model.ExchangeValue;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepository extends JpaRepository<ExchangeValue,Long> {
-    ExchangeValue findByFromAndTo(String from, String to);
-}
